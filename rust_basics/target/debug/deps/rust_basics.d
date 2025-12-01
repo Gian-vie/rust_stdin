585af4e706c456b4d57e0e018f3cb8c2e5fafa52
Gian Vieceli <gianvieceli@gmail.com>
2025-11-30 23:42:55 -0300

    estrutura de projeto com o cargo

---
+++ rust_basics/target/debug/deps/rust_basics.d
@@ -0,0 +1,5 @@
+C:\Users\gianv\OneDrive\Documentos\GitHub\rust_stdin\rust_basics\target\debug\deps\rust_basics.d: src\main.rs
+
+C:\Users\gianv\OneDrive\Documentos\GitHub\rust_stdin\rust_basics\target\debug\deps\rust_basics.exe: src\main.rs
+
+src\main.rs:
---
+++ rust_basics/target/debug/rust_basics.d
@@ -0,0 +1,1 @@
+C:\Users\gianv\OneDrive\Documentos\GitHub\rust_stdin\rust_basics\target\debug\rust_basics.exe: C:\Users\gianv\OneDrive\Documentos\GitHub\rust_stdin\rust_basics\src\main.rs
