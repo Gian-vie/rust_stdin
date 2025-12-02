659201834f82ed421970e7108a504aa01a8c35c3
Gian Vieceli <gianvieceli@gmail.com>
2025-12-01 23:53:31 -0300

    api Hello Whorld! em RUST

--- rust_basics/src/main.rs
+++ rust_basics/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+async fn root() -> &'static str {
+    "Hello World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("servidor rodando na porta http://{}", addr);
+
+    let listener = match TcpListener::bind(addr).await {
+        Ok(l) => l,
+        Err(e) => {
+            eprintln!("Erro ao blindar a porta : {}", e);
+            return;
+        }
+    };
+
+    if let Err(e) = axum::serve(listener, app).await {
+        eprintln!("Erro no servidor: {}", e);
+    }
 }
